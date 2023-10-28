a33d3e7e895cc3b54d26e52a78d91df6abf0b5b2
Ahmet Yanık <ahmetyaniklar@gmail.com>
2023-10-28 21:36:54 +0300

    SideBar Next.js version update

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,8 +1,10 @@
-import { HomeIcon,MagnifyingGlassIcon,BuildingLibraryIcon } from '@heroicons/react/24/solid'
+'use client'
+
+import { HomeIcon,MagnifyingGlassIcon,BuildingLibraryIcon, HeartIcon, RssIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
 
 function Sidebar() {
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900" >
         <div>
             <button className='flex items-center space-x-2 hover:text-white'>
             <HomeIcon className="h-5 w-5"/>
@@ -16,7 +18,22 @@ function Sidebar() {
             <BuildingLibraryIcon className='h-5 w-5'/>
             <p>Your Library</p>
             </button>
-            <hr className='border-t-[0.1px]'/>
+            <hr className='border-t-[0.1px] border-gray-900'/>
+
+
+            <button className='flex items-center space-x-2 hover:text-white'>
+            <PlusCircleIcon className="h-5 w-5"/>
+            <p>Create Playlist</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white'>
+            <HeartIcon className='h-5 w-5'/>
+            <p>Your Library</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white'>
+            <RssIcon className='h-5 w-5'/>
+            <p>Your episodes</p>
+            </button>
+            <hr className='border-t-[0.1px] border-gray-900'/>
         </div>
     </div>
   )
