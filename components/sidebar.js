1e3775102e36aef261fe64c718327a403d7d2c0f
Ahmet Yanık <ahmetyaniklar@gmail.com>
2023-10-29 17:50:49 +0300

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,7 +5,7 @@ import { HomeIcon,MagnifyingGlassIcon,BuildingLibraryIcon, HeartIcon, RssIcon, P
 function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900" >
-        <div>
+        <div className='space-y-4'>
             <button className='flex items-center space-x-2 hover:text-white'>
             <HomeIcon className="h-5 w-5"/>
             <p>Home</p>
@@ -34,6 +34,15 @@ function Sidebar() {
             <p>Your episodes</p>
             </button>
             <hr className='border-t-[0.1px] border-gray-900'/>
+
+            {/* Playlist */}
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+            <p className='cursor-pointer hover:text-white '> Playlist Name</p>
+
         </div>
     </div>
   )
